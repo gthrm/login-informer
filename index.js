0bc81fdf3bb405145accb122d3048fedd8f048c5
cdROma <mrv24ru@yandex.ru>
2022-04-05 02:32:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import { Telegraf } from 'telegraf'
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const chatId = process.env.CHAT_ID
 const serverName = process.env.SERVER_NAME
-const [ip] = process.argv.slice(2);
+const [ip, user] = process.argv.slice(2);
 
-const message = `Login to ${serverName} from IP: ${ip}, ${new Date()}`
+const message = `Login to ${serverName} by user: ${user} from IP: ${ip}, ${new Date()}`
 
 bot.telegram.sendMessage(chatId, message);
