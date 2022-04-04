93f4d6e49f0fbe28fef6a5a285c676197382bdd4
cdROma <mrv24ru@yandex.ru>
2022-04-05 02:43:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const chatId = process.env.CHAT_ID;
 const serverName = process.env.SERVER_NAME;
 const [ip, user] = process.argv.slice(2);
 
-const message = `Login to ${serverName} by user: ${user} from IP: ${ip}, ${new Date()}`;
+const message = `Login to ${serverName} has been started.\nUser: ${user}\nIP: ${ip}\n${new Date()}`;
 
 bot.telegram.sendMessage(chatId, message);
